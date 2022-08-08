fa0aad7c90db3dd63b3f9a811917c613b934f2b6
Bobok Elena <lenabobok@mail.ru>
2022-08-08 22:28:52 +0700

    задание 3-8.08

--- task_3/solution.js
+++ task_3/solution.js
@@ -1,15 +1,17 @@
 function renderCartItem(item) {
-    const product = item;
+   // const product = item;
 
     // Задание №3.1. Формирование строки корзины
 
-    product = {id: id, title: title, count: count, price: price}
+    product = [
+        {id: 1, title: "Пицца", count: 1, price: 500},
+        {id: 2, title: "Чебурек", count: 2, price: 120}];
     let itemCountText = '';
     itemCountText = itemCountText + product.count;
-    itemCountText = itemCountText + "*";
+    itemCountText = itemCountText + "×";
     itemCountText = itemCountText + product.price;
     itemCountText = itemCountText + "₽ = ";
-    let sum = product.count * product.price;  
+    let sum = product.count × product.price;  
     itemCountText = itemCountText + sum;
     itemCountText = itemCountText  + "₽";
 
