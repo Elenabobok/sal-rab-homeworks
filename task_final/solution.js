982ec04d824a2395dc270159e29713f8008932d5
Bobok Elena <lenabobok@mail.ru>
2022-08-16 08:11:42 +0700

    Итоговое задание (исправление1)

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,7 +34,7 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: '${name} ${phone}',order: {address: 'ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}',sum}, goods: []};
+    let data = {client: `${name} ${phone}`,order: {address: `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`,sum}, goods: []};
     
     for (let i in goods) {
         data.goods.push({title: goods[i].title,count: goods[i].count});
